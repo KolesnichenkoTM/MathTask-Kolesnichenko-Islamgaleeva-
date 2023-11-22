767b88fa238a0a2dc851dd60d05f2299bbf2daa3
KolesnichenkoTM <151588820+KolesnichenkoTM@users.noreply.github.com>
2023-11-22 13:28:57 +1000

    Add files via upload

---
+++ Geometry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathTask_Kolesnichenko_Islamgaleeva_
+{
+    public class Geometry
+    {
+        public double RectangleArea(int a, int h)
+        {
+            return Math.Round(Math.PI * (a * a) * h);
+
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MathTask_Kolesnichenko_Islamgaleeva_;
+
+
+namespace MathTaskTest_Kolesnichenko_Islamgaleeva_
+{
+    [TestClass]
+    public class GeometryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 4;
+            int h = 5;
+            int expected = 251;
+            Geometry g = new Geometry();
+            double actual = g.RectangleArea(a, h);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
